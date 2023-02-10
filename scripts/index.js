2a9ef83e6b7a458f7d2d01d9de56aca7eaac1150
Zack <zdove@live.co.uk>
2023-02-10 17:15:55 +0000

    Fem's changes

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,17 @@ const quoteGlider = new Glide('.quotesContainer>.glide', {
   gap: 0,
   perView: isDesktop ? 2 : 1,
   bound: isDesktop,
+  autoplay: isDesktop ? false : 3000,
 }).mount()
 quoteGlider.on('resize', () => {
   console.log('resize')
   quoteGlider.update({
     perView: window.matchMedia("(min-width: 600px)").matches ? 2 : 1,
     bound: window.matchMedia("(min-width: 600px)").matches,
+    autoplay: window.matchMedia("(min-width: 600px)").matches ? false : 3000,
   })
 })
+
 
 const photoGlider = new Glide('.photoGlider>.glide', {
   gap: 0,
@@ -32,8 +35,7 @@ class LoopingText {
     this.lerp = { current: 0, target: 0 };
     this.interpolationFactor = 0.1;
     this.speed = 0.1;
-    this.direction = -1; // -1 (to-left), 1 (to-right) 
-
+    this.direction = el.classList.contains('reverse') ? -1 : 1; // -1 (to-left), 1 (to-right) 
     // Init
     this.el.style.cssText = `position: relative; display: inline-flex; white-space: nowrap;`;
     this.el.children[1].style.cssText = `position: absolute; left: ${100 * -this.direction}%;`;
@@ -99,7 +101,7 @@ function setActivePreviousEdition(year) {
   console.log(target)
 
   for (let i = 0; i < tabs.length; i++) {
-    overlayTop.appendChild(tabs[i])
+    // overlayTop.appendChild(tabs[i])
     tabs[i].classList.remove('active');
   }
   const targetTab = document.getElementById('tab' + year.toString());
@@ -109,7 +111,7 @@ function setActivePreviousEdition(year) {
   targetTab.classList.add('active');
 
   for (let i = 0; i < titles.length; i++) {
-    overlayTop.appendChild(titles[i])
+    // overlayTop.appendChild(titles[i])
     titles[i].classList.remove('active');
   }
   const targetTitle = document.getElementById('title' + year.toString());
@@ -119,3 +121,15 @@ function setActivePreviousEdition(year) {
 
 
 }
+
+function closeMailer(){
+  console.log('close mailer')
+
+  const mailerContainer  =  document.getElementById('mailerContainer')
+  mailerContainer.classList.add('hide')
+  setTimeout( ( ) => {
+    mailerContainer.remove()
+  }, 2000)
+}
+
+document.querySelector('#titleContainer>video').loop = true;
