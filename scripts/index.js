2b32a029d390db1d674aa60b81342edf819b5d26
Zack <zdove@live.co.uk>
2023-02-10 13:59:01 +0000

    First draft done

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,22 @@
-new Glide('.glide', {
+const isDesktop = window.matchMedia("(min-width: 600px)").matches;
+
+const quoteGlider = new Glide('.quotesContainer>.glide', {
   gap: 0,
+  perView: isDesktop ? 2 : 1,
+  bound: isDesktop,
+}).mount()
+quoteGlider.on('resize', () => {
+  console.log('resize')
+  quoteGlider.update({
+    perView: window.matchMedia("(min-width: 600px)").matches ? 2 : 1,
+    bound: window.matchMedia("(min-width: 600px)").matches,
+  })
+})
+
+const photoGlider = new Glide('.photoGlider>.glide', {
+  gap: 0,
+  animationDuration: 1000,
+  autoplay: 5000,
 }).mount()
 
 
@@ -12,11 +29,11 @@ const lerp = (current, target, factor) => current * (1 - factor) + target * fact
 class LoopingText {
   constructor(el) {
     this.el = el;
-    this.lerp = {current: 0, target: 0};
+    this.lerp = { current: 0, target: 0 };
     this.interpolationFactor = 0.1;
-    this.speed = 0.2;
+    this.speed = 0.1;
     this.direction = -1; // -1 (to-left), 1 (to-right) 
-    
+
     // Init
     this.el.style.cssText = `position: relative; display: inline-flex; white-space: nowrap;`;
     this.el.children[1].style.cssText = `position: absolute; left: ${100 * -this.direction}%;`;
@@ -25,13 +42,16 @@ class LoopingText {
   }
 
   events() {
-    window.addEventListener("scroll", () => this.lerp.target += this.speed * 5);
+    document.getElementById('mainContentContainer').addEventListener("scroll", () => { 
+      console.log('hi')
+      this.lerp.target += this.speed * 5 
+    });
   }
 
   animate() {
     this.lerp.target += this.speed;
     this.lerp.current = lerp(this.lerp.current, this.lerp.target, this.interpolationFactor);
-    
+
     if (this.lerp.target > 100) {
       this.lerp.current -= this.lerp.target;
       this.lerp.target = 0;
@@ -49,37 +69,53 @@ class LoopingText {
 
 document.querySelectorAll(".marqueeContainer").forEach(el => new LoopingText(el));
 
-function setActivePreviousEdition(year){
+const tabs = [
+  document.getElementById('tab2019'),
+  document.getElementById('tab2018'),
+  document.getElementById('tab2017'),
+  document.getElementById('tab2016'),
+  document.getElementById('tab2015'),
+]
+
+const titles = [
+  document.getElementById('title2019'),
+  document.getElementById('title2018'),
+  document.getElementById('title2017'),
+  document.getElementById('title2016'),
+  document.getElementById('title2015'),
+]
+
+function setActivePreviousEdition(year) {
   console.log(year)
   const overlayTop = document.getElementById('overlayTop')
 
   const previousEditions = document.querySelectorAll('.previousEditions>div');
   console.log(previousEditions)
-  for (let i = 0; i < previousEditions.length; i++){
+  for (let i = 0; i < previousEditions.length; i++) {
     previousEditions[i].classList.remove('active');
   }
   const target = document.getElementById(year.toString());
   target.classList.add('active')
   console.log(target)
 
-  const previousTabs = document.querySelectorAll('.overlayTop>path');
-  for (let i = 0; i < previousTabs.length; i++){
-    previousTabs[i].classList.remove('active');
+  for (let i = 0; i < tabs.length; i++) {
+    overlayTop.appendChild(tabs[i])
+    tabs[i].classList.remove('active');
   }
-  const targetTab = document.getElementById('tab'+year.toString());
+  const targetTab = document.getElementById('tab' + year.toString());
   overlayTop.appendChild(targetTab)
-  
-  console.log('tab'+year.toString())
+
+  console.log('tab' + year.toString())
   targetTab.classList.add('active');
 
-  const previousTitles = document.querySelectorAll('.overlayTop>text');
-  for (let i = 0; i < previousTitles.length; i++){
-    previousTitles[i].classList.remove('active');
+  for (let i = 0; i < titles.length; i++) {
+    overlayTop.appendChild(titles[i])
+    titles[i].classList.remove('active');
   }
-  const targetTitle = document.getElementById('title'+year.toString());
+  const targetTitle = document.getElementById('title' + year.toString());
   targetTitle.classList.add('active');
   overlayTop.appendChild(targetTitle)
 
-  
+
 
 }
