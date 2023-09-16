3e60af645da7cffb3c496a3e595dd2c92ba65d6b
Zack <zdove@live.co.uk>
2023-09-16 14:24:08 +0200

    Updated styling for mobile

--- zd24/simple-things-2024/scripts/index.js
+++ zd24/simple-things-2024/scripts/index.js
@@ -1,5 +1,16 @@
 const menu = document.getElementById("menu");
+const lineup = document.getElementById("lineup");
 
 function toggleMenu() {
   menu.classList.toggle("show");
+}
+
+function closeMenu() {
+  menu.classList.remove("show");
+}
+
+function scrollToLineup() {
+  console.log("scroll to lineup");
+  closeMenu();
+  lineup.scrollIntoView({ behavior: "smooth" });
 }
